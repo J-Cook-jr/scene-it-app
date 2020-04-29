cfd7bd3e586ca22eeeade6ccc0c6420b9271fafa
Jeff Cook <jecook2692@gmail.com>
2020-04-29 14:51:17 -0400

    adds updates to js and html

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const myForm = document.getElementById(‘myForm’);
-myForm.addEventListener(‘submit’, function(e){
+const myForm = document.getElementById('search-form');
+myForm.addEventListener('submit', function (e) {
     e.preventDefault();
+    function renderMovies(movieArray) {
+        let movieHTMLArray = movieArray.map(function (currentMovie) {
+            return `
+            <div class="col-4 mb-4">
+                <div class="card h-100">
+                    <img src="${currentMovie.Poster}" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5>${currentMovie.Title}<span class="badge badge-secondary">${currentMovie.Year}</span></h5>
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-outline-success">Save</button>
+                    </div>
+                </div>
+            </div>
+            `
 
+        });
+        return movieHTMLArray.join('');
+    }
+    const moviesContainer = document.querySelector('.movies-container')
+    const moviesRow = document.querySelector('.row')
+
+    moviesContainer.innerHTML = renderMovies(movieData);
 })
